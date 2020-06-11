955d202d06b93941880cd175788f823cbc13b023
Dimitris <divergos@gmail.com>
2020-06-11 18:20:48 +0300

    Routes implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 require('./model/User');
+require('./model/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:1234@cluster0-6qmtw.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
@@ -21,7 +25,7 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', () => {
     console.log('Error Connection to mongo', err);
 });
-app.get('/', (req, res) => {
+app.get('/', requireAuth, (req, res) => {
     res.send('Hi There!');
 });
 
